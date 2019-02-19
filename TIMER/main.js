e33d9238fb51601a42900e976b1572a967440641
Raghav Dhingra <raghav.dhingra15@gmail.com>
2019-02-20 00:40:03 +0530

    Add files via upload

--- TIMER/main.js
+++ TIMER/main.js
@@ -11,3 +11,18 @@ const move = () => {
         btn_count=0;
         document.getElementById('btn').innerHTML='Start Counter';
     }
+    //counter
+    var x = setInterval(timer,1000);
+    function timer(){
+        if (btn_count==1){
+            counter+=1;
+            document.getElementById('count_1').innerText=counter;
+            if (counter!=0){
+                document.getElementById('reset_btn').disabled=false;
+            }
+            }
+            else if (btn_count==0) {
+                clearInterval(x);
+            }
+        }
+    }
